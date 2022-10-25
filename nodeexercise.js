74f473b0d033e938b0ac84155ddc5b6085b996d8
Adriana Almeida <adriana.wd@outlook.com>
2022-10-25 17:45:37 +1300

    updating with NodeJs Testing and JSON exercises

--- nodeexercise.js
+++ nodeexercise.js
@@ -1,6 +1,35 @@
+//Nodejs Exercise
 const animals = ["dog", "cat", "elephant", "lion", "donkey"];
 
 animals[1] = "turtle";
 animals[4] = "zebra";
 
 console.log(animals);
+
+
+//NodeJS and JSON Exercise
+
+const book = {
+    "title" : "First JSON Exercise",
+    "description" : "This is my first JSON exercise during my Software Engineering Course at IOD",
+    "author" : "Adriana Silva Almeida",
+    "number_of_pages" : "12"
+}
+
+console.log(book)
+
+//NodeJS Testing Exercise
+
+//Testing
+if(sum(2,4)!=6){
+    console.log("The test failed");
+} else {
+    console.log("The test passed");
+}
+
+//function
+function sum(a,b){
+    return a+b
+    }
+
+console.log(sum(2,2))
