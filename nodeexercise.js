d7daa4e0f94a02468cbda65cf8d361efc969bcf8
Adriana Almeida <adriana.wd@outlook.com>
2022-10-27 10:02:01 +1300

    updating nodeexercise.js with test exercise

--- nodeexercise.js
+++ nodeexercise.js
@@ -20,16 +20,50 @@ console.log(book)
 
 //NodeJS Testing Exercise
 
-//Testing
-if(sum(2,4)!=6){
-    console.log("The test failed");
-} else {
-    console.log("The test passed");
-}
-
 //function
 function sum(a,b){
-    return a+b
-    }
+    return a+b;
+}
 
-console.log(sum(2,2))
+//testing
+if(sum(2,4)!=6){
+    console.log("sum -> Test 1: Failed");
+} else {
+    console.log("sum -> Test 1: Passed")
+}
+
+if (sum(10,20) == 30){
+    console.log('sum -> Test 2 : Passed');
+}else {
+    console.log('sum -> Test 2 : Failed')
+}
+
+if (sum(20) == 20){
+    console.log('sum -> Test 3 : Passed');
+}else{
+    console.log('sum -> Test 3 : Failed')
+}
+
+if (sum(20, 0) == 20){
+    console.log('sum -> Test 4 : Passed');
+}else{
+    console.log('sum -> Test 4 : Failed')
+}
+
+if (sum(20, 'ABC') == 20){
+    console.log('sum -> Test 5 : Passed');
+}else{
+    console.log('sum -> Test 5 : Failed')
+}
+
+if (sum(20, 0/10) == 20){
+    console.log('sum -> Test 6 : Passed');
+}else{
+    console.log('sum -> Test 6 : Failed')
+}
+
+if (sum(20, 1/0) == 20){
+    console.log('sum -> Test 7 : Passed');
+}else{
+    console.log('sum -> Test 7 : Failed')
+}
